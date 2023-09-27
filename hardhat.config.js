b4fc49f253d479a96ac6bc21ace09080ea689d91
durga chaitanya <chaitutatipamula023@gmail.com>
2023-09-28 01:48:58 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks : {
+    swisstronik : {
+      url:  process.env.ALCHEMY_RPC_API_KEY,
+      accounts : [process.env.PRIVATE_KEY]
+    }
+  }
+};
